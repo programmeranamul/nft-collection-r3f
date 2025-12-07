6b64ed98c6457ab3eb734103ba6e410d82d8db70
programmeranamul <programmer.anamul@gmail.com>
2025-12-08 00:09:53 +0600

    added screenshot script

--- src/App.jsx
+++ src/App.jsx
@@ -5,10 +5,17 @@ import Experience from "./components/Experience";
 function App() {
   return (
     <>
-      <Canvas camera={{ position: [0, 0, 4], fov: 45 }}>
-        <color  attach="background" args={["#222222"]}/>
+      <Canvas
+        camera={{ position: [0, 0, 4], fov: 45 }}
+        gl={{ preserveDrawingBuffer: true }}
+        // gl={{ preserveDrawingBuffer: true }}
+      >
+        <color attach="background" args={["#222222"]} />
         <Experience />
       </Canvas>
+      <button className="generate" onClick={() => window.takeScreenshort()}>
+        Generate collection
+      </button>
     </>
   );
 }
--- src/components/Experience.jsx
+++ src/components/Experience.jsx
@@ -1,7 +1,52 @@
 import { Center, OrbitControls } from "@react-three/drei";
 import { Pirate } from "./Pirate";
+import { useThree } from "@react-three/fiber";
+import { useEffect } from "react";
+
+const Stage = () => {
+  return (
+    <>
+      <mesh rotation={[-0.5 * Math.PI, 0, 0]} position={[0, -1, 0]}>
+        <planeGeometry args={[10, 10, 1, 1]} />
+        <meshBasicMaterial transparent opacity={0.1} color={"black"} />
+      </mesh>
+
+      <mesh rotation={[0, 0, 0]} position={[0, 0, -4]}>
+        <planeGeometry args={[10, 10, 1, 1]} />
+        <meshBasicMaterial color={"#00b4ff"} />
+      </mesh>
+
+      <mesh receiveShadow position={[1.5, 1.5, -1]}>
+        <circleGeometry args={[1, 64]} />
+        <meshBasicMaterial color={"yellow"} transparent opacity={0.82} />
+      </mesh>
+    </>
+  );
+};
 
 export default function Experience() {
+  const gl = useThree((state) => state.gl);
+
+  const screenshot = () => {
+    console.log("kuttar bacca")
+    const link = document.createElement("a");
+    link.setAttribute("download", "canvas.png");
+    link.setAttribute(
+      "href",
+      gl.domElement
+        .toDataURL("image/png")
+        .replace("image/png", "image/octet-stream")
+    );
+
+    link.click();
+  };
+
+useEffect(() => {
+    // 💡 Add the function to the global scope on mount
+    window.takeScreenshort = screenshot;
+
+  }, []);
+
   return (
     <>
       <OrbitControls />
@@ -12,6 +57,7 @@ export default function Experience() {
       <Center>
         <Pirate />
       </Center>
+      <Stage />
     </>
   );
 }
